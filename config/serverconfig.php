1c6fca4d1042b4fe96fe994c0d55a6cc465fcd0c
shezars <shezars@gmail.com>
2017-08-24 21:15:59 +0700

    updated

--- config/serverconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-$database_host = "localhost";
-$database_username = "root";
-$database_password = "";
-$database_name = "dbforum";
-$db=new ee_database($database_host,$database_username,$database_password,$database_name);
-?>
